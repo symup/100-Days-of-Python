405de38e31d76c364f5ac788bb8723ef87b1913c
Phung S Villalobos <57890868+symup@users.noreply.github.com>
2021-01-02 20:03:56 -0800

    Update 2-Heads or Tails exercise.py

--- Day 4/2-Heads or Tails exercise.py
+++ Day 4/2-Heads or Tails exercise.py
@@ -1,1 +1,16 @@
+#You are going to write a virtual coin toss program. 
+#It will randomly tell the user "Heads" or "Tails".
+# e.g.
+# 1 means Heads
+# 0 means Tails
 
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+
+random_integer = random.randint(0,1)
+
+if random_integer == 1:
+        print("Heads")
+else:
+        print("Tails")
