17303e217590299be27126fe548514f30dcb4c9f
Phung S Villalobos <57890868+symup@users.noreply.github.com>
2021-01-03 20:21:39 -0800

    Update 3. High score.py

--- Day 5/3. High score.py
+++ Day 5/3. High score.py
@@ -15,3 +15,12 @@ for score in student_scores:
   if score > highest_score:
     highest_score = score
 print(f"The highest score in the class is: {highest_score}")
+
+
+OUTPUT:
+  
+  '''
+Input a list of student scores 78 65 89 86 55 91 64 89
+[78, 65, 89, 86, 55, 91, 64, 89]
+The highest score in the class is: 91
+  '''
