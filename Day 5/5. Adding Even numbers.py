36ac5ea8e0c60ca1158ef1afeb92544c61b97bea
Phung S Villalobos <57890868+symup@users.noreply.github.com>
2021-01-04 14:42:46 -0800

    Create 5. Adding Even numbers.py

---
+++ Day 5/5. Adding Even numbers.py
@@ -0,0 +1,16 @@
+# You are going to write a program that calculates the sum of all the even numbers from 1 to 100, including 2 and 100.
+
+#Write your code below this row 👇
+
+#calcutate total using range function
+total = 0
+for number in range(2,101,2):
+    total += number
+print(total)
+
+
+OUTPUT:
+
+'''
+2550
+'''
