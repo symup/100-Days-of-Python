9e82f5c0990a1d83a5b4564cadd052331a23c439
Phung S Villalobos <57890868+symup@users.noreply.github.com>
2021-01-05 16:20:56 -0800

    Update 2. Exercise-Average Height.py

--- Day 5/2. Exercise-Average Height.py
+++ Day 5/2. Exercise-Average Height.py
@@ -1,3 +1,5 @@
+AVERAGE HEIGHT
+
 # You are going to write a program that calculates the average student height from a List of heights.
 # e.g. student_heights = [180, 124, 165, 173, 189, 169, 146]
 
