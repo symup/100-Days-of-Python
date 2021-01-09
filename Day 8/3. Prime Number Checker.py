653bbd1d9fe1e0280f9a1d9cd0e9c4a0d88b479f
Phung S Villalobos <57890868+symup@users.noreply.github.com>
2021-01-08 16:36:48 -0800

    Update 3. Prime Number Checker.py

--- Day 8/3. Prime Number Checker.py
+++ Day 8/3. Prime Number Checker.py
@@ -1,1 +1,29 @@
 PRIME NUMBER CHECKER
+
+#Prime numbers are numbers that can only be cleanly divided by itself and 1.
+
+#Write your code below this line 👇
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            #not prime
+            is_prime = False
+    if is_prime:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
+
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
+
+
+OUTPUT:
+  
+'''
+Check this number: 75
+It's not a prime number.
+'''
