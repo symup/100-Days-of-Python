bdb553b67328273c7d9bd38ce395c0ee1123864f
Phung S Villalobos <57890868+symup@users.noreply.github.com>
2021-01-10 17:28:58 -0800

    Update 4D. Caesar Cipher (final).py

--- Day 8/4D. Caesar Cipher (final).py
+++ Day 8/4D. Caesar Cipher (final).py
@@ -13,9 +13,6 @@ def caesar(start_text, shift_amount, cipher_direction):
     for char in start_text:
         if char in alphabet:
             #TODO-3: What happens if the user enters a number/symbol/space?
-            #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
-            #e.g. start_text = "meet me at 3"
-            #end_text = "•••• •• •• 3"
             position = alphabet.index(char)
             new_position = position + shift_amount
             end_text += alphabet[new_position]
@@ -28,9 +25,6 @@ from art import logo
 print(logo)
 
 #TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
-#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
-#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
-#Hint: Try creating a new function that calls itself if they type 'yes'. 
 should_continue = True #flag
 while should_continue:
 
